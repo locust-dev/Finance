7d6c32082741dedcc7d916ec639ff8a052549d9f
locust-dev <rrrr.ty30@gmail.com>
2021-02-04 19:43:27 +0300

    Initial Commit

---
+++ Finance/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Finance
+//
+//  Created by Илья Тюрин on 04.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

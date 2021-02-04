a02dcc63185bda21662ef74b7cfcf15b85dc8e8a
locust-dev <rrrr.ty30@gmail.com>
2021-02-04 20:27:29 +0300

    Start developing

--- Finance/VC_add.swift
+++ Finance/VC_add.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  VC_add.swift
 //  Finance
 //
 //  Created by Илья Тюрин on 04.02.2021.
@@ -7,15 +7,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class VC_add: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
-
-    @IBAction func addValue(_ sender: UIButton) {
+    
+    @IBOutlet weak var howMuchOutlet: UITextField!
+    
+    @IBAction func howMuch(_ sender: UITextField) {
     }
     
 }
-
---
+++ Finance/VC_category.swift
@@ -0,0 +1,39 @@
+//
+//  VC_category.swift
+//  Finance
+//
+//  Created by Илья Тюрин on 04.02.2021.
+//
+
+import UIKit
+
+class VC_category: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    var howMuch: Double = 0
+    @IBOutlet weak var howMuchLabel: UILabel!
+    
+    
+    
+    @IBAction func cat1(_ sender: Any) {
+    }
+
+    @IBAction func cat2(_ sender: Any) {
+    }
+    
+
+    @IBAction func cat3(_ sender: Any) {
+    }
+    
+    @IBAction func cat4(_ sender: Any) {
+    }
+    
+    @IBAction func accept(_ sender: Any) {
+    }
+}
